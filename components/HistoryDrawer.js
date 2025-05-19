3742c8da9fb31755866b443984e208da41b76dec
selector7 <pdss666666@gmail.com>
2025-05-19 13:28:58 +0800

    Create HistoryDrawer.js

---
+++ components/HistoryDrawer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HistoryDrawer = () => {
+  return (
+    <div className="glass p-4 rounded-lg m-4 w-full max-w-md">
+      <h3 className="text-lg font-bold mb-2">运动历史</h3>
+      <p>暂无数据</p>
+    </div>
+  );
+};
+
+export default HistoryDrawer;
