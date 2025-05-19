11a2ae131257787d29fb1457e3eb4be2101bbca2
selector7 <pdss666666@gmail.com>
2025-05-19 13:26:10 +0800

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="p-4 text-white">
+      <h1 className="text-2xl font-bold">运动数据面板</h1>
+    </div>
+  );
+};
+
+export default Header;
