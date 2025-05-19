dd64934d62e6a907e204463d4d8bd8d4e8a6e18f
selector7 <pdss666666@gmail.com>
2025-05-19 13:25:15 +0800

    Create Background.js

---
+++ components/Background.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Background = () => {
+  return <div className="fixed w-full h-full bg-gradient-to-b from-blue-500 to-purple-500" />;
+};
+
+export default Background;
